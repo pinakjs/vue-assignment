206eb9beff976ea65e93d0eba970e5e7428e416d
pinak <webtricks.ak@gmail.com>
2024-03-03 13:12:30 +0530

    created base button component

--- src/App.vue
+++ src/App.vue
@@ -1,12 +1,23 @@
 <script setup lang="ts">
-    
+import { ref } from 'vue';
+import BaseButton from './components/BaseButton.vue';
+
+const name = ref("World!")
+
+const reverseName = () => {
+    name.value = name.value.split("").reverse().join("")
+}
+
+const showAlert = () => {
+    alert("This is an alert")
+}
 </script>
 
 <template>
-    <h1>Hello World!</h1>
-    
+    <h1>Hello {{  name  }}</h1>
+    <base-button @click="showAlert" value="Show Alert" background-color="#e0a800" text-color="#fff" /> 
+    <base-button @click="reverseName"  value="Reverse Name" background-color="#c823333" text-color="#e0a800" /> 
 </template>
-
 <style>
 
 </style>
---
+++ src/components/BaseButton.vue
@@ -0,0 +1,24 @@
+<script setup lang="ts">
+defineProps<{
+    backgroundColor: string,
+    textColor: string,
+    value: string        
+}>()
+
+defineEmits(['click']);
+</script>
+
+<template>
+    <button
+        :style="{ backgroundColor, color: textColor }"
+        type="button"
+        title="Click on Button"
+        @click="$emit('click')"
+    >
+        {{ value  }}
+    </button>
+</template>
+
+<style>
+
+</style>
