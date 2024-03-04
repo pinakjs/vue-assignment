dab21a2768db113b32edd041b0c19fd3b3ed899c
pinak <webtricks.ak@gmail.com>
2024-03-04 10:15:31 +0530

    created card component

--- src/App.vue
+++ src/App.vue
@@ -1,12 +1,39 @@
 <script setup lang="ts">
-    
+    import Card from './components/Card.vue';
+    import Image from './assets/peakpx.jpg'
 </script>
 
 <template>
     <h1>Hello World!</h1>
-    
+    <Card />
+
+    <Card>
+        <template #header>
+            <img :src="Image" alt="Buttefly" />
+        </template>
+        <template #title>
+            A Butterfly
+        </template>
+        <template #body>
+            <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Harum facere...</p>
+        </template>
+        <template #footer>
+            <button type="button" title="Click here">
+                See more
+            </button>
+        </template>
+    </Card>
 </template>
 
-<style>
+<style scoped>
+    img {
+        max-width: 100%;
+        border-radius: 5px;
+        user-select: none;
+    }
 
+    p {
+        font-size: 0.9rem;
+        margin-top: 0.5rem;
+    }
 </style>
---
+++ src/components/Card.vue
@@ -0,0 +1,86 @@
+<script setup lang="ts">
+
+</script>
+
+<template>
+    <div class="card">
+        <div class="card__header">
+            <slot name="header">
+                300 * 100
+            </slot>
+        </div>
+
+        <div class="card__body">
+            <h5>
+                <slot name="title">
+                    A sample title
+                </slot>
+            </h5>
+            <slot name="body">
+                <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Beatae aspernatur voluptates soluta sunt debitis quae.</p>
+            </slot>
+        </div>
+
+        <div class="card__footer">
+            <slot name="footer">
+                <button class="button" title="click for more info">More Info</button>
+            </slot>
+        </div>
+    </div>    
+</template>
+
+<style scoped>
+    .card {
+        background: #fff;
+        padding: 0.5rem;
+        display: block;
+        width: 100%;
+        max-width: 180px;
+        border-radius: 5px;
+        margin: 0.8rem;
+        box-shadow: 0 0 15px 2px rgba(0, 0, 0, 0.15);
+    }
+
+    .card__header {
+        width: 100%;
+        aspect-ratio: 3 / 2;
+        display: flex;
+        justify-content: center;
+        align-items: center;
+        font-size: 1.4rem;
+        font-weight: bold;
+        background-color: rgb(249, 127, 255);
+        color: #fff;
+        border-radius: 5px;
+    }
+
+    .card__body {
+        width: 100%;
+    }
+    
+    .card__body h5 {
+        font-size: 1.2rem;
+        margin-top: 0.5rem;
+        text-align: left;
+        margin-bottom: 0.4rem;
+    }
+
+    .card__body p {
+        font-size: 0.9rem;
+        margin-top: 0.4rem;
+        color: rgb(46, 54, 57);
+    }
+
+    .card__footer {
+        width: 100%;
+        margin-top: 0.6rem;
+    }
+
+    .card__footer button {
+        font-size: 0.9rem;
+        width: 100%;
+        background-color: rgb(170, 78, 236);
+        color: #fff;
+        padding: 0.4rem 0.6rem;
+    }
+</style>
